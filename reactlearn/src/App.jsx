03502385e7baedce7b8bac2f557dec8144bf1a99
abdul-rehman07 <abdulrehmankiyani7@gmail.com>
2024-07-30 17:00:30 +0500

    new react vite

---
+++ reactlearn/src/App.jsx
@@ -0,0 +1,14 @@
+import Learn from "./learn"
+
+function App() {
+
+  return (
+    <>
+<Learn/>
+<p>i am using laptop</p>
+<span>working</span>
+    </>
+  )
+}
+
+export default App
---
+++ reactlearn/src/learn.jsx
@@ -0,0 +1,6 @@
+function Learn(){
+    return(
+        <h3>Hello World</h3>
+    )
+}
+export default Learn;
