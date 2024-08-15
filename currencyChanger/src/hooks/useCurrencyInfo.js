15cb2c01f7557f929dbad95454562a261f4191e1
abdul-rehman07 <abdulrehmankiyani7@gmail.com>
2024-08-15 18:07:31 +0500

    works

---
+++ currencyChanger/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react";
+
+function useCurrencyInfo(currency) {
+  const [data, setData] = useState({});
+  useEffect(() => {
+    fetch(`https://6674243575872d0e0a95448f.mockapi.io/v1/${currency}`)
+      .then((res) => res.json())
+      .then((res) => setData(res[currency]));
+    console.log(data);
+  }, [currency]);
+  console.log(data);
+  return data;
+}
+
+export default useCurrencyInfo;
