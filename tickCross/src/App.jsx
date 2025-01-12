f7354b96b7c1fe0e5545c51b0db4bb5b030280bd
abdul-rehman07 <abdulrehmankiyani7@gmail.com>
2025-01-12 15:58:42 +0500

    drives states

--- tickCross/src/App.jsx
+++ tickCross/src/App.jsx
@@ -1,11 +1,30 @@
 import Player from "./assets/player"
 import GameBoard from "./assets/GameBoard.jsx"
 import { useState } from "react"
+import Log from "./assets/Log.jsx";
 
 function App() {
+  const [gameTruns, setGameTruns] = useState([]);
   const [activePlayer, setActivePlayer] = useState('X');
-  function handleSelectSqure() {
+
+
+  function handleSelectSqure(rowIndex, colIndex) {
     setActivePlayer((curactiveplayer) => curactiveplayer === 'X' ? 'O' : 'X')
+    setGameTruns((prevTruns) => {
+      let currentPlayer = 'X';
+
+      if (prevTruns.length > 0 && prevTruns[0].player === 'X') {
+        currentPlayer = 'O';
+      }
+
+      const updatedTruns = [
+        { square: { row: rowIndex, col: colIndex }, player: activePlayer },
+        ...prevTruns,
+      ];
+
+      return updatedTruns;
+    }
+    )
   }
   return (
     <>
@@ -16,8 +35,10 @@ function App() {
             <Player initialName='player 1' symbol='X' isActive={activePlayer === 'X'} />
             <Player initialName='player 2' symbol='O' isActive={activePlayer === 'O'} />
           </ol>
-          <GameBoard onSelectSqure={handleSelectSqure} activePlayerSymbol={activePlayer} />
+          <GameBoard onSelectSqure={handleSelectSqure}
+            truns={gameTruns} />
         </div>
+        <Log />
       </main>
 
     </>
--- tickCross/src/assets/GameBoard.jsx
+++ tickCross/src/assets/GameBoard.jsx
@@ -1,23 +1,31 @@
-import { useState } from "react";
-
 const initialGameboard = [
     [null, null, null],
     [null, null, null],
     [null, null, null],
 ];
 
-export default function ({ onSelectSqure, activePlayerSymbol }) {
-    const [gameBoard, setgameBoard] = useState(initialGameboard);
+export default function ({ onSelectSqure, truns }) {
+    let gameBoard = initialGameboard;
+    for (const trun of truns) {
+        const { squre, player } = trun;
+        const { row, col } = squre;
 
-    function handleselectedbtn(rowIndex, colIndex) {
-        setgameBoard((prevBtn) => {
-            const updatebtn = [...prevBtn.map(innerArray => [...innerArray])]
-            updatebtn[rowIndex][colIndex] = activePlayerSymbol;
-            return updatebtn;
-        });
+        gameBoard[row][col] = player;
 
-        onSelectSqure();
+
     }
+
+    // const [gameBoard, setgameBoard] = useState(initialGameboard);
+
+    // function handleselectedbtn(rowIndex, colIndex) {
+    //     setgameBoard((prevBtn) => {
+    //         const updatebtn = [...prevBtn.map(innerArray => [...innerArray])]
+    //         updatebtn[rowIndex][colIndex] = activePlayerSymbol;
+    //         return updatebtn;
+    //     });
+
+    //     onSelectSqure();
+    // }
 
     return (
         <ol id="main-gameboard">
@@ -26,7 +34,7 @@ export default function ({ onSelectSqure, activePlayerSymbol }) {
                     <ol>
                         {row.map((playerSymbol, colIndex) => (
                             <li key={colIndex}>
-                                <button onClick={() => handleselectedbtn(rowIndex, colIndex)}>{playerSymbol}</button>
+                                <button onClick={() => onSelectSqure(rowIndex, colIndex)}>{playerSymbol}</button>
                             </li>
                         ))}
                     </ol>
