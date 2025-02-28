7787b9c1b812f5b2bb2e81c07e4ed42838987edf
abdul-rehman07 <abdulrehmankiyani7@gmail.com>
2025-02-28 18:23:22 +0500

    tailwind css

--- management/src/App.jsx
+++ management/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
+import NewProject from './Components/NewProject'
 import ProjectSidebar from './Components/ProjectSidebar'
 
 function App() {
 
   return (
-    <main>
+    <main className='h-screen my-8 flex gap-8'>
       <ProjectSidebar />
+      <NewProject />
     </main>
   )
 }
