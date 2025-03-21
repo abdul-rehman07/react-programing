587d0f553f4ba4ef6806a31ba13347d6dd16a2fe
abdul-rehman07 <abdulrehmankiyani7@gmail.com>
2025-03-21 16:50:38 +0500

    use effects

--- placeHolder/src/App.jsx
+++ placeHolder/src/App.jsx
@@ -1,8 +1,8 @@
 import { useRef, useState, useEffect } from 'react';
 import Places from './components/Places.jsx';
 import { AVAILABLE_PLACES } from './data.js'
-import Modal from './components/Modal.jsx';
-import DeleteConfirmation from './components/DeleteConfirmation.jsx';
+import Modal from './Components/Modal.jsx';
+import DeleteConfirmation from './Components/DeleteConfirmation.jsx';
 import logoImg from './assets/logo.png';
 import { sortPlacesByDistance } from './loc.js';
 
--- placeHolder/src/Components/DeleteConfirmation.jsx
+++ placeHolder/src/Components/DeleteConfirmation.jsx
@@ -1,4 +1,17 @@
+import { useEffect } from "react";
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
+  useEffect(() => {
+    console.log('time set');
+    const timer = setTimeout(() => {
+      onConfirm();
+    }, 3000);
+    return () => {
+      console.log('cleaning up timing');
+      clearTimeout(timer)
+    }
+  }, []);
+
+
   return (
     <div id="delete-confirmation">
       <h2>Are you sure?</h2>
